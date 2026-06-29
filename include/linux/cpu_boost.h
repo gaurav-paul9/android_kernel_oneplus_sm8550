7802f46f27f0cff8cfcb1acfa528775a48a8e6be
gaurav-paul9 <paulgaurav9@gmail.com>
2026-06-29 09:58:16 +0530

    drivers/cpufreq: add CPU Boost driver
    Port the CPU frequency boost driver from YAAP waffle (SM8650) to SM8550.
    Provides two functions:
      - cpu_boost_max(ms): forces all CPUs to max freq for duration
      - cpu_boost_kick(ms): boosts CPUs to configurable kick kHz when
        recent touch input detected (within 5s window)
    
    Cluster mapping adapted for Kalama: CPUs 0-2 (little/A510),
    CPUs 3-6 (mid/A710), CPU 7 (big/X3).
    
    Uses standard freq_qos API for cpufreq policy constraints and
    input_handler for touch detection.
    
    Signed-off-by: luna <luna@bydaniel.org>

---
+++ include/linux/cpu_boost.h
@@ -0,0 +1,16 @@
+#ifndef _CPU_BOOST_H
+#define _CPU_BOOST_H
+
+#if IS_ENABLED(CONFIG_CPU_BOOSTING)
+void cpu_boost_max(unsigned int duration_ms);
+void cpu_boost_kick(unsigned int duration_ms);
+#else
+static inline void cpu_boost_max(unsigned int duration_ms)
+{
+}
+static inline void cpu_boost_kick(unsigned int duration_ms)
+{
+}
+#endif
+
+#endif /* _CPU_BOOST_H */
